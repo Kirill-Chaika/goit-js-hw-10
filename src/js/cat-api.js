50cd0909f8ed02592e1429f5faca8758d21674ad
Kirill-Chaika <115115398+Kirill-Chaika@users.noreply.github.com>
2023-07-02 14:06:30 +0300

    ввава

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,17 +1,21 @@
-export const fetchCountries = name => {
-  const BASE_URL = 'https://api.thecatapi.com/v1/images/search';
-  const properties = {
-    id,
-    url,
-    breeds,
-    favourite
-    };
-    const API_Key = { 'x-api-key' : 'live_qX3jDfl6LZPHo4tDqz6q5vYSj5jz1oaxYAyuWKQJvqhuR1sOgNl1Mq09li2F8LNh' };
-  return fetch(`${BASE_URL}${name}?${API_Key}${properties}`).then(response => {
-     console.log(response)
-     if (!response.ok) {
-         throw new Error(response.status);
-     }
-     return response.json();
- });
-};
+
+import axios from "axios";
+
+axios.defaults.headers.common["x-api-key"] = "live_qX3jDfl6LZPHo4tDqz6q5vYSj5jz1oaxYAyuWKQJvqhuR1sOgNl1Mq09li2F8LNh";
+
+const BASE_URL = 'https://api.thecatapi.com/v1';
+
+
+
+fetch(`${BASE_URL}/breeds`)
+.then(res => {
+  if(!res.ok){
+    throw new Error(res.status);
+  }
+  return res.json();
+}).then(data => {
+  console.log(data)
+})
+.catch(err => {
+  console.log(err);
+});
