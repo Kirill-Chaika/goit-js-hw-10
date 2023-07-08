7bd9a64b666bf05bdf761730a445bd043acfc2fa
Kirill-Chaika <115115398+Kirill-Chaika@users.noreply.github.com>
2023-07-08 12:09:37 +0300

    фывфыв

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -2,10 +2,10 @@
 import axios from "axios";
 
 axios.defaults.headers.common["x-api-key"] = "live_qX3jDfl6LZPHo4tDqz6q5vYSj5jz1oaxYAyuWKQJvqhuR1sOgNl1Mq09li2F8LNh";
-
 const BASE_URL = 'https://api.thecatapi.com/v1';
 
 const axios = require('axios');
+
 
 // Make a request for a user with a given ID
 axios.get(`${BASE_URL}/breeds`)
